26b69cc400300dd727444b950fe331d3f95b69a6
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-11 16:57:47 +0200

    [UPD] modified cocktails controller

--- app/Http/Controllers/Guest/CocktailsController.php
+++ app/Http/Controllers/Guest/CocktailsController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
 use Illuminate\Http\Request;
 
 class CocktailsController extends Controller
 {
     public function index()
     {
-        // $cocktails = Cock
+        $cocktails = Cocktail::all();
+
+        return view('welcome', compact('cocktails'));
     }
 }
