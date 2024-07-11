38cef20aa206ab1ee24d61bbf60857399a9e3c9c
annaritadituri <annaritadituri@icloud.com>
2024-07-11 17:16:37 +0200

    [UPD] Basic layout

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand">Bar</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
-    @vite('resources/js/app.js')
-</head>
+@section('main-home')
 
-<body>
-    <div class="container py-5">
-        <header>
-            <div class="d-flex justify-content-center">
-                <h1>Template Laravel</h1>
-            </div>
-        </header>
+    <div class="container my-5">
+
+        <h1 class="text-center my-5">Cocktails</h1>
+
+        <div class="row g-3">
+
+            @for ($i = 0; $i < 6; $i++)
+                <div class="col-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+            
+        </div>
     </div>
-</body>
 
-</html>
+@endsection
