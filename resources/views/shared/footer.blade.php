dc1b191bef8eb19056c114579fb6362657196161
annaritadituri <annaritadituri@icloud.com>
2024-07-11 17:16:37 +0200

    [UPD] Basic layout

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+
+    <nav class="navbar sticky-bottom bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Torna alla ricerca</a>
+        </div>
+    </nav>
+    
+</footer>
