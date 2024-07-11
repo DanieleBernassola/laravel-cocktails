44ce285a3cf91494314509a80f3ad8c02ec1602c
MiriamPecora <miriampecora97@gmail.com>
2024-07-11 17:21:57 +0200

    [ADD] Added seeder and used faker to add elements to db

---
+++ database/seeders/CocktailsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cocktail;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class CocktailsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        $ingredientsList = [
+            'Rum', 'Vodka', 'Gin', 'Tequila', 'Whiskey', 'Lime', 'Lemon', 'Mint', 'Sugar', 'Soda', 'Cola', 'Orange Juice', 'Pineapple Juice', 'Grenadine'
+        ];
+
+        for ($i = 0; $i < 10; $i++) {
+            $cocktail = new Cocktail();
+
+            $cocktail->name = $faker->word . ' ' . $faker->colorName;
+            $cocktail->ingredients = implode(', ', $faker->randomElements($ingredientsList, $faker->numberBetween(3, 5)));
+            $cocktail->price = $faker->randomFloat(2, 5, 20);
+            $cocktail->gradation = $faker->randomFloat(2, 0, 40);
+            $cocktail->is_alcoholic = $faker->boolean();
+
+            $cocktail->save();
+        }
+    }
+}
+
