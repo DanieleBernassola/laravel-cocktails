e605e7cd0df12ff05ff29a2a225c435ae7b93868
annaritadituri <annaritadituri@icloud.com>
2024-07-22 18:12:25 +0200

    [UPD] Style homepage

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,11 +1,11 @@
 <header>
     
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar py-4">
         <div class="container">
-            <a class="navbar-brand">Bar</a>
+            <a class="navbar-brand text-white position-relative" href="#">LISTA Bar</a>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <button class="btn btn-primary" type="submit">Search</button>
             </form>
         </div>
     </nav>
