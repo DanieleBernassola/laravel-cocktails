10f59d0a3a5773e4bcd7cd586ae6b0228a075205
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-22 16:38:41 +0200

    [UPD] Created file added

--- resources/views/cocktails/create.blade.php
+++ resources/views/cocktails/create.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.backoffice')
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
     <div class="container my-5">
 
         <h1 class="text-center my-5">Cocktails</h1>
@@ -30,3 +41,8 @@
     </div>
 
 @endsection
+
+
+
+</body>
+</html>
