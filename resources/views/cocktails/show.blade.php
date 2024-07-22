f2a4b26097c66e7371322b46d3e0c33ccc174627
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-22 18:29:34 +0200

    [UPD] Modified text color

--- resources/views/cocktails/show.blade.php
+++ resources/views/cocktails/show.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.backoffice')
 
 @section('content')
-    <div class="container my-5">
+    <div class="container">
+        <div class="row text-black">
+            <h2 class="text-center my-5">Informazioni cocktail</h2>
+            <div class="col mb-5">
+                <h3>Nome: {{ $cocktail->name }}</h3>
+                <p>Ingredienti: {{ $cocktail->ingredients }}</p>
+                <p>Prezzo: {{ $cocktail->price }}</p>
+                @if ($cocktail->is_alcoholic)
+                    <p>ALCOLICO</p>
+                    <p>Gradazione alcolica: {{ $cocktail->gradation }}</p>
+                @endif
 
-        <h2 class="text-center my-5">Informazioni cocktail</h2>
-
-        <h3>Nome: {{ $cocktail->name }}</h3>
-        <p>Ingredienti: {{ $cocktail->ingredients }}</p>
-        <p>Prezzo: {{ $cocktail->price }}</p>
-        @if ($cocktail->is_alcoholic)
-            <p>ALCOLICO</p>
-            <p>Gradazione alcolica: {{ $cocktail->gradation }}</p>
-        @endif
-
-        <a href="{{ route('cocktails.index') }}">Torna all'elenco</a>
-        <a href="{{ route('cocktails.edit', $cocktail->id) }}">Modifica</a>
+                <a href="{{ route('cocktails.index') }}">Torna all'elenco</a>
+                <a href="{{ route('cocktails.edit', $cocktail->id) }}">Modifica</a>
+            </div>
+        </div>
 
     </div>
     </div>
