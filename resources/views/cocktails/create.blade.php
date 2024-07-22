e863b349ee52e27cc2b5219472a71b90279a8eec
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-22 16:24:01 +0200

    [UPD] Files created

---
+++ resources/views/cocktails/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.backoffice')
+
+
+    <div class="container my-5">
+
+        <h1 class="text-center my-5">Cocktails</h1>
+
+        <div class="row g-3">
+
+            @foreach ($cocktails as $cocktail)
+
+                <div class="col-4">
+                    <div class="card position-relative h-100">
+                        @if ($cocktail->is_alcoholic == 1)
+                            <span class="alcoholic p-1 rounded">ALCOHOLIC</span>
+                        @endif
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyATogFnv4VSmjGKaiJlEl_GY6bddoxE9hRQ&s" class="card-img-top" alt="cocktail">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $cocktail->name }}</h5>
+                            <p class="card-text">Ingredients: {{ $cocktail->ingredients }}</p>
+                            <p class="card-text">Price: &euro;{{ $cocktail->price }}</p>
+                            <p class="card-text">Gradation: {{ $cocktail->gradation }}&percnt; </p>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+            
+        </div>
+    </div>
+
+@endsection
